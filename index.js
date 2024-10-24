a6f5c9edcb7a55d585e28feb3580b44aaef8e59e
kaizen <boshesap123625@gmail.com>
2024-10-24 18:52:25 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const db = require('./databasecode.js');
+
+console.log(`Welcome! You can use these for variables:
+db.setVar('VariableName', 'VariableValue', 'UserID (or global)'); // Sets variable's value
+db.fetchVar('VariableName'); // Gets variable's value
+db.createVar('VariableName'); // Creates variable`);
