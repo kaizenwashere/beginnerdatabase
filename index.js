74a211402f115c831910d868f11d804a54f65b3d
kaizen <boshesap123625@gmail.com>
2024-10-25 19:05:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,35 +5,30 @@ function createVar(varname) {
     const variablesPath = path.join(__dirname, 'variables.json');
 
     if (!fs.existsSync(variablesPath)) {
-        console.error(`Error: ${variablesPath} not found.`);
         return;
     }
 
     let variables = JSON.parse(fs.readFileSync(variablesPath, 'utf8'));
 
     if (variables[varname] !== undefined) {
-        console.log(`Variable "${varname}" already exists in variables.json.`);
         return;
     }
 
     variables[varname] = "";
 
     fs.writeFileSync(variablesPath, JSON.stringify(variables, null, 2), 'utf8');
-    console.log(`Variable "${varname}" added to variables.json.`);
 }
 
 function fetchVar(varname, userid = 'global') {
     const variablesPath = path.join(__dirname, 'variables.json');
 
     if (!fs.existsSync(variablesPath)) {
-        console.error(`Error: ${variablesPath} not found.`);
         return null;
     }
 
     let variables = JSON.parse(fs.readFileSync(variablesPath, 'utf8'));
 
     if (!variables[varname]) {
-        console.log(`Variable "${varname}" not found in variables.json.`);
         return null;
     }
 
@@ -49,7 +44,6 @@ function setVar(varname, varvalue, userid = 'global') {
     const variablesPath = path.join(__dirname, 'variables.json');
 
     if (!fs.existsSync(variablesPath)) {
-        console.error(`Error: ${variablesPath} not found.`);
         return;
     }
 
@@ -62,7 +56,6 @@ function setVar(varname, varvalue, userid = 'global') {
     variables[varname][userid] = varvalue;
 
     fs.writeFileSync(variablesPath, JSON.stringify(variables, null, 2), 'utf8');
-    console.log(`Variable "${varname}" set to "${varvalue}" for userid "${userid}" in variables.json.`);
 }
 
 module.exports = {
